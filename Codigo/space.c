7a58654944cc60c4d15e41b53ef453f93f095064
Juan <superjuan260@hotmail.com>
2016-10-04 00:58:25 +0200

    mas cambios en space

--- Codigo/space.c
+++ Codigo/space.c
@@ -1,20 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "space.h"
+
 struct space_{
 	int sId;
 	int neighbour[8];
 	char *shortDesc;
 	char *longDesc;
-	bool light;
-	bool isLocked[8];
+	Bool light;
+	Bool isLocked; 
+	/*cambio importante: aquello que hablamos de que si una habitacion estaba cerrada, lo estaba entraras desde donde 
+	entraras. En vez de un array de isLocked con cada una de las posibles direcciones, 
+	ahora es una propiedad de la propia habitacion (tal vez deberiamos consultarlo con el resto)
+	Esto repercute en (y lo cierto es que complica un poco, aunque sigo pensando qut tal vez sea lo mas eficiente) 
+	la implementacion de: isLocked y unlock*/
 	char ** map;
 	int rows, cols;
 }Space;
 
+/*-----------------------Funciones sin terminar-----------------------*/
+
+int unlock(Space *s, int dir, Bool status){
+	Space * spaceToUnlock = NULL;
+	if(!s||dir<0||dir>7) return -1;
+	spaceToUnlock = /*funcion que devuelva un puntero al space on Id s->neighbor[dir]*/
+	if(!spaceToUnlock) return -1
+	if(spaceToUnlock->isLocked==status) return 1;
+	if(/*player no tiene la llave*/) return -2;
+	spaceToUnlock->isLocked;
+	return 0;
+}
+
+
+
+Space * spaceIni(){
+
+}
+
+void spaceFree(Space *s){
+
+}
+
+Bool isLocked(Space *s, int dir){
+	Space * spaceToGo = NULL;
+	if(!s||dir<0||dir>7) return FALSE;
+	spaceToGo = /*funcion que dado un spaceId devuelva un  puntero a ese space, esta funcion debe de estar en el world creo*/
+	return spaceToGo->isLocked;
+}
+
+char **whatIsInMap(Space *c, int *s){
+
+	/*LUCIA ¿para que sirve la s?*/
+}
+
+
+/*------------------Funciones minigame------------------*/
+
+int exitMgame(Space *s){
+	return 0;
+}
+
+/*------------------Fuciones terminadas-----------------*/
+
 char *sDesc(Space * s){
+	if (!s) return NULL;
 	return s->shortDesc;
 }
 
 char *lDesc(Space * s){
+	if (!s) return NULL;
 	return s->lDesc;
 }
 
-int space_ neighbour( Space *s, int n)
+int go(Space *s, int dir){
+	if (!s||sir<0||dir>7) return NULL;
+	return s->neighbour[dir];
+}
+
+
+
+Bool canISee(Space *s){
+	if(!s) return FALSE;
+	return s->light;
+}
+
+int nCols(Space *s){
+	if(!s) return -1;
+	return s->rows;
+}
+
+int nRows(Space *s){
+	if(!s) return -1;
+	return s->rows;
+}
+
+int spaceId(Space *s){
+	if(!s) return -1;
+	return s->sId;
+}
+
+
+
--- Codigo/space.h
+++ Codigo/space.h
@@ -1,17 +1,17 @@
 #ifndef SPACE_H
 #define SPACE_H
-
+#include "types.h"
 typedef struct space_ Space;
 
 
 /*Creates a new space, allocating memory, NULL in case of error*/
-Space * space_ini();
+Space * spaceIni();
 
 /*Frees memory allocated for a space and its members*/
-void space_free(Space *s);
+void spaceFree(Space *s);
 
 /*Returns sId of s, -1 in  case of error*/
-int space_id(Space *s);
+int spaceId(Space *s);
 
 /*Returns sId of the chosen s neighbour (minigame case 4:7). -1 in case of error*/
 int space_ neighbour( Space *s, int n);
@@ -29,10 +29,13 @@ propiedades (light, neighbour como bien has dicho tampoco tiene sentido...) Igua
 ya pensaremos como implementar mas adelante cuando vayamos pensando como van a funcionar los minijuegos (que van a tener una tela...) 
 El minigame es muy parecido a un space en el sentido de que, al menos por ahora tendría un spaceId igual que un space, pero detrás de éste
 spaceId se encuentra algo distinto a un space normal. ¿Que opinas?
+
+Tambien he cambiado el formato de algunas funciones para que todas tengan el mismo formato: primeraPalabraMinusculaYMayusculaLasDemas
+Si prefieres la barra baja lo podemos cambiar tambien :D
 */
 
 /*Takes the player back to the minigame "parent" space, returning parent sId, -1 in case of error*/
-int exit_mgame(Space *s);
+int exitMgame(Space *s);
 
 /*Returns short description of s, NULL in case of error*/
 char *sDesc(Space * s);
@@ -41,20 +44,20 @@ char *sDesc(Space * s);
 char *lDesc(Space * s);
 
 /*Returns 0 if the chosen s neigbour is unlocked, 1 if its locked*/
-bool is_locked(Space *s, int neigbour)
+Bool isLocked(Space *s, int dir)
 
 /*Returns char map of the specified space, NULL in case of error*/
-char **whatisinmap(Space *c, int *s);
+char **whatIsInMap(Space *c, int *s);
 
 /*Returns 0 if s is dark, 1 if not*/
 /*To use "bool, true, false" instead of "_Bool, 1, 0" : <stdbool.h> has to be included*/
-bool canIsee(Space *s);
+Bool canISee(Space *s);
 
 /*Returns number of columns of s map, -1 error*/
-int ncols(Space *s);
+int nCols(Space *s);
 
 /*Returns number of rows of s map, -1 error*/
-int nrows(Space *s);
+int nRows(Space *s);
 
 /*Up to here, we have the ini/free functions and the ones to access all members of struct*/
 
@@ -62,8 +65,8 @@ int nrows(Space *s);
 int go(Space *s, int dir);
 
 /*Status tells us whether the user wants to lock or unlock door
- Returns 0 if succesfully (un)locked, -1 in case of no space, -2 if no key*/
-int unlock(Space *s, int dir, bool status);
+ Returns 0 if succesfully (un)locked, -1 in case of no space, -2 if no key and 1 if it is already locked or unlocked*/
+int unlock(Space *s, int dir, Bool status);
 
 
 #endif
