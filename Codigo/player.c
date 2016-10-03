af84ffac2352ff8508be52eb82a0b23c81cc99d4
carlosisamar <carlosisamar@gmail.com>
2016-10-04 00:12:17 +0200

    Update player.c
    en proceso

--- Codigo/player.c
+++ Codigo/player.c
@@ -3,7 +3,9 @@
 #include <assert.h>
 #include <string.h>
 #define name(p) (p)->name
-
+#define spaceid(p) (p)->spaceid
+#define x(p) (p)->coordx
+#define y(p) (p)->coordy
 
 struct _Player{
 	char *name;
@@ -11,3 +13,35 @@ struct _Player{
 	int coordx;
 	int coordy;
 };
+
+Player *player_ini(){
+	Player *p;
+	p = (player*) malloc(sizeof(Player));
+	if(p == NULL) return NULL;
+	name(p) = NULL;
+	spaceid(p) = 0;
+	x(p) = 0;
+	y(p) = 0;
+	return p;
+}
+
+void player_free(Player *p){
+	assert (p != NULL);
+	if (name(p) != NULL) free(name(p));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
