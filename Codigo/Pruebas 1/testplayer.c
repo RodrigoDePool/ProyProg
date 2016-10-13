6a44cd6c624c227ea896cc667a18684b5961865c
carlosisamar <rodrigodp05@gmail.com>
2016-10-13 20:00:54 +0200

    Revisado comentarios (estaba todo bien) y probado testplayer.c

---
+++ Codigo/Pruebas 1/testplayer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "player.c"
+#include <string.h>
+
+int main(){
+	Player *p = player_ini();
+	char name[] = "TestingName";
+	char *nameret;
+	
+	Player_setName(p, name);
+	Player_setSpaceid(p, 1);
+	Player_setCoordinateX(p, 2);
+	Player_setCoordinateY(p, 3);
+	nameret = Player_getName(p);
+	if (strcmp(nameret, name) == 0)
+		printf("set and get name: OK\n");
+	if (Player_getSpaceid(p) == 1)
+		printf("set and get spaceid: OK\n");
+	if (Player_getCoordinateX(p) == 2)
+		printf("set and get coordinate x: OK\n");
+	if (Player_getCoordinateY(p) == 3)
+		printf("set and get coordinate y: OK\n");
+	player_free(p);
+	return 1;
+}
+
