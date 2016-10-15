d54e107363c462ea490834f2fc326af71dbd9d56
luciaasen <luciaasen@gmail.com>
2016-10-15 16:10:36 +0200

    Corrijo space.c, añado prueba a spacefromfile y añado a notas los archivos de Santini de interfaces

---
+++ Codigo/Pruebas 1/spacefiletest/spacefromfile.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "space.h"
+
+void main(){
+	FILE *f;
+	Space *s;
+	int i;
+	
+	f = fopen("spacetest.txt", "r");
+	s = spacefromfile(f);
+	if(!s)
+		printf("Algo salió mal");
+	else
+		printf("%d\n", space_getId(s));
+		for(i = 0; i<7; i++)
+			printf("%d ", space_getNeighbour(s, i));
+		printf("%d\n", space_getNeighbour(s, i));
+		
+		printf("%s\n%s\n%d\n%d\n%d %d\n", space_getSDesc(s), space_getLDesc(s), 
+		      space_canISee(s), space_isLocked(s), space_getNRows(s),
+		      space_getNCols(s));
+		for(i = 0; i<space_getNRows; i++){
+			printf("%s\n", space_getMap(s)[i]);
+		}
+	space_free(s);
+	fclose(f);
+	return;
+}
