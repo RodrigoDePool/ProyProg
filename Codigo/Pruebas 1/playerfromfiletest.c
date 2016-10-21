7ee555f2990fec96b29e9b933ea2fc34f7d5377d
Carlos Ignacio Isasa Martin <carlosisamar@gmail.com>
2016-10-21 16:53:26 +0200

    playertestfromfile OK

--- Codigo/Pruebas 1/playerfromfiletest.c
+++ Codigo/Pruebas 1/playerfromfiletest.c
@@ -1,5 +1,6 @@
 #include "player.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 	FILE *f;
@@ -13,17 +14,17 @@ int main(){
 	
 	p = playerfromfile(f);
 	name = Player_getName(p); 
-	if (name == nametest)
-		printf ("Name: OK");
+	if (strcmp(name, nametest) == 0)
+		printf ("Name: OK\n");
 	else printf ("ERROR NAME\n");
 	if (Player_getSpaceid(p) == 1)
-		printf ("Spaceid: OK");
+		printf ("Spaceid: OK\n");
 	else printf ("ERROR SPACE\n");
 	if (Player_getCoordinateX(p) == 2)
-		printf ("X: OK");
+		printf ("X: OK\n");
 	else printf ("ERROR X\n");
 	if (Player_getCoordinateY(p) == 3)
-		printf ("Y: OK");
+		printf ("Y: OK\n");
 	else printf ("ERROR Y\n");
 	player_free(p);
 	return 1;
