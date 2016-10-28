97f0740a603d056ce184d2ebe6cec61d869beb8d
RodrigoDePool <rodrigodp05@gmail.com>
2016-10-28 17:36:50 +0200

    Comentado i_create

--- Codigo/interface.h
+++ Codigo/interface.h
@@ -4,8 +4,14 @@
 typedef struct _Interface Interface;
 
 /*	Creates an interface:
-	Parameters: bc colums of the board (usable board -2)
-
+	Parameters: bc colums of the board (usable bc -2)
+					The other two are used for margins
+				br rows of the board (usuable br -2)
+				dc colums of display...
+				dr rows of display...
+				cc columns of commandline...
+				cr rows of ...
+	return: Pointer to Interface initialized
 */
 Interface *i_create(int bc, int br, int dc, int dr, int cc, int cr);
 
