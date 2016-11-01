dec1ead4faa13ffc1941403d7eebc0c66ffcb323
Juan <superjuan260@hotmail.com>
2016-11-02 00:13:20 +0100

    Intrface

--- Codigo/interface.c
+++ Codigo/interface.c
@@ -13,9 +13,32 @@ struct _Interface{
 	int cc;/*columns in command*/
 	int cr;/*rows in command*/
 
+	int bbkcl;/*background color of the board*/
+	int bfgcl;/*foreground color of the board*/
+	int dbkcl;/*background color of the board*/
+	int dfgcl;/*foreground color of the board*/
+	int cbkcl;/*background color of the board*/
+	int cfgcl;/*foreground color of the board*/
+
 	char  cplayer;/*character of the player*/
 
 
 };
+
 Interface *i_create(int bc, int br, int dc, int dr, int cc, int cr
-					, char cplayer);
+					, char cplayer, int bbkcl, int bfgcl, int dbkcl, int dfgcl, 
+					int cbkcl, int cfgcl){
+
+	Interface * interface;
+	interface=(Interface*)malloc(sizeof(Interface));
+	iterface->bc=bc;
+	interface->br=br;
+	interface->dc=dc;
+	interface->dr=dr;
+	interface->cc=cc;
+	interface->cr=cr;
+	interface->cplayer=cplayer;
+
+	interface->board=win_new()
+	return interface;
+}
