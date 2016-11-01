26475d9c0c23b60a5b518e54adb2e02f6f33cb42
RodrigoDePool <rodrigodp05@gmail.com>
2016-11-01 17:09:05 +0100

    Agregadas funciones a implementar en interface.h y creado el test parcialmente

---
+++ Codigo/Pruebas 1/interfacetest/interfacetest.c
@@ -0,0 +1,103 @@
+#include <stdlib.h>
+#include "interface.h"
+
+/*
+  Initializes the terminal in such a way that we can read the input
+  without echo on the screen
+*/
+
+struct termios initial;
+
+void _term_init() {
+	struct termios new;	          /*a termios structure contains a set of attributes about
+					  how the terminal scans and outputs data*/
+
+	tcgetattr(fileno(stdin), &initial);    /*first we get the current settings of out
+						 terminal (fileno returns the file descriptor
+						 of stdin) and save them in initial. We'd better
+						 restore them later on*/
+	new = initial;	                      /*then we copy them into another one, as we aren't going
+						to change ALL the values. We'll keep the rest the same */
+	new.c_lflag &= ~ICANON;	              /*here we are setting up new. This line tells to stop the
+						canonical mode (which means waiting for the user to press
+						enter before sending)*/
+	new.c_lflag &= ~ECHO;                 /*by deactivating echo, we tell the terminal NOT TO
+						show the characters the user is pressing*/
+	new.c_cc[VMIN] = 1;                  /*this states the minimum number of characters we have
+					       to receive before sending is 1 (it means we won't wait
+					       for the user to press 2,3... letters)*/
+	new.c_cc[VTIME] = 0;	              /*I really have no clue what this does, it must be somewhere in the book tho*/
+	new.c_lflag &= ~ISIG;                 /*here we discard signals: the program won't end even if we
+						press Ctrl+C or we tell it to finish*/
+
+	tcsetattr(fileno(stdin), TCSANOW, &new);  /*now we SET the attributes stored in new to the
+						    terminal. TCSANOW tells the program not to wait
+						    before making this change*/
+}
+
+
+/*
+  Reads a key from the keyboard. If the key is a "regular" key it
+  returns its ascii code; if it is an arrow key, it returns one of the
+  four interface directions with the "minus" sign
+*/
+int _read_key() {
+  char choice;
+  choice = fgetc(stdin);
+
+
+  if (choice == 27 && fgetc(stdin) == '[') { /* The key is an arrow key */
+    choice = fgetc(stdin);
+
+    switch(choice) {
+    case('A'):
+      return NORTH;
+    case('B'):
+      return SOUTH;
+    case('C'):
+      return EAST;
+    case('D'):
+      return WEST;
+    default:
+      return HERE;
+    }
+  }
+  else
+    return choice;
+}
+
+
+
+int main(){
+  Interface *i;
+  int c;
+  _term_init();/*Initializes the terminal for the interface*/
+  i=i_create(/*PONER PARAMETROS*/);
+
+  i_drawPl(i,2,2);
+
+  while(1){
+      c= _read_key();
+      if (c == 'q') {
+        tcsetattr(fileno(stdin), TCSANOW, &initial);	/*We now restore the settings we back-up'd
+                                                      so that the termial behaves normally when
+                                                      the program ends */
+        i_free(i);
+        return 0;
+      }
+      move(i,c);
+  }
+  
+  i_free(i);
+  return 0;
+
+}
+/*CODIGO UTIL
+int c = _read_key();
+if (c == 'q') {
+  tcsetattr(fileno(stdin), TCSANOW, &initial);	/*We now restore the settings we back-up'd
+            so that the termial behaves normally when
+            the program ends */
+/*  exit(0);
+}
+*/
--- Codigo/interface.h
+++ Codigo/interface.h
@@ -1,11 +1,17 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
+#define HERE 0
 #define WEST 1
 #define EAST 2
 #define NORTH 3
 #define SOUTH 4
+#include <stdlib.h>
+#include <stdio.h>
+#include "rectangles.h"
 
 typedef struct _Interface Interface;
+
+
 
 /*	Creates an interface.
 	Parameters: bc colums of the board (usable bc -2)
@@ -23,21 +29,60 @@ Interface *i_create(int bc, int br, int dc, int cr, char cplayer,
 					int cbkcl, int cfgcl);
 
 
+
 /*
 	Draws the Interface in the terminal
 	Parameter: i Pointer to Interface
 	returns: OK or ERR
 */
-Status draw(Interface *i);
+Status i_drawAll(Interface *i);
+
+
+
+/*
+	Draws a characrer in the board.
+	Parameters:
+		i Pointer to interface
+		c character thats gonna be written
+		br row in the board you wanna write the character in
+		bc column in the board you wanna write the character in
+	returns: OK or ERR
+*/
+Status i_drawChar(Interface *i, char c, int br, int bc);
+
+
+
+/*
+	Initilizes the position of the player in the board and draw it
+	Paramters:
+		i Pointer to interface
+		br row in the board the player is in
+		bc column in the board the player is in
+	returns: OK or ERR
+*/
+Status i_drawPl(Interface *i,int br, int bc);
 
 
 /*
 	Moves the player in the map to the direction introduced
-	Parameter: 
+	Parameter:
 		i pointer to interface
 		dir integer with the direction to which you ll move
 	returns: OK if it moved or ERR if its not possible to move
+	ATTENTION THE PLAYER MUST HAVE BEEN INITIALIZED FIRST
 */
 Status move(Interface *i,int dir);
+
+
+/*
+	frees an interface
+	Parameters:
+		Pointer to interface
+	Does not clear the interface, it leaves it as it was before calling the
+	function, you must clear it in the main, see : interfacetest.c
+
+*/
+void i_free(Interface *i);
+
 
 #endif
