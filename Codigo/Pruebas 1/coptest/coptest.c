4c71367cfc5a895c635b020437476c285c257b1e
luciaasen <luciaasen@gmail.com>
2016-11-25 13:15:02 +0100

    Coñazo

--- Codigo/Pruebas 1/coptest/coptest.c
+++ Codigo/Pruebas 1/coptest/coptest.c
@@ -1,8 +1,11 @@
+
+/*Incluyo .c en vez de .h para poder acceder a los campos de las structs*/
 #include "cop.h"
 
 int main(){
 	CoP* c;
 	FILE *f;
+	int i, j;
 	
 	f = fopen("cop.txt", "r");
 	if(!f) return -1;
@@ -12,9 +15,21 @@ int main(){
 		return -1;
 	}
 	
-	assoc
+	/*Comprobamos que dentro del cop creado esta la informacion que quiero*/
+	printf("Número de exts (debería ser 3): %d\n", c->numexts);
+	
+	for(i = 0; i < c->numexts; i++){
+		printf("Ext nº %d:\n", i+1);
+		printf("\tExt_name: %s\n", c->exts[i]->ext_name);
+		printf("\tInt_name: %s\n", c->exts[i]->int_name);
+		printf("\tN_ans: %d\n", c->exts[i]->n_ans);
+		for(j = 0; j < c->exts[i]->n_ans; j++){
+			printf("\t\t%dº ans: %s\n", j+1, c->exts[i]->ans[j]);
+		}
+	}
 	
 	
+	fclose(f);
 	cop_free(c);	
 }
 
