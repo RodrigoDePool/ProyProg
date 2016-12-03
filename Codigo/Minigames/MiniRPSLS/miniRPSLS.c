39f5b69790e90b4795dea29d416efd46347c44b0
RodrigoDePool <rodrigodp05@gmail.com>
2016-12-03 20:57:03 +0100

    Terminado RPSLS

--- Codigo/Minigames/MiniRPSLS/miniRPSLS.c
+++ Codigo/Minigames/MiniRPSLS/miniRPSLS.c
@@ -1,17 +1,15 @@
 #include "miniRPSLS.h"
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 #define rock 1
 #define scissors 2
 #define paper 3
 #define lizzard 4
 #define spock 5
+#define LEN 40
+#define lines 4
 
-int round(Interface *i);
-int miniRPSLS(Interface *i){
-  i_readFile(i,INI_PATH,0,0,1);
-  return 0;
-}
 
 
 
@@ -24,13 +22,14 @@ int miniRPSLS(Interface *i){
   returns 2 it tie
   returns -1 if error
 */
-int round(Interface *i){
+int _round(Interface *i){
   int ran,des;
 
 
   do{
     des=fgetc(stdin);
-  }while(des==1 || des ==2 || des==3 || des==4 || des==5);
+    des=des-'0';
+  }while(des!=1 && des !=2 && des!=3 && des!=4 && des!=5);
 
   srand(time(NULL));
   ran = rand() % 5 + 1;
@@ -100,4 +99,93 @@ int round(Interface *i){
       return 0;
   }
   return -1;
+}
+
+
+/*
+  Function writes in the pointer passed the lines the game is gonna write in
+  the command line, given in ROUNDS_PATH
+*/
+void read_sols(char **s){
+  FILE *f;
+  int i;
+  f=fopen(ROUND_PATH,"r");
+  if(f==NULL)
+    return;
+  for(i=0;i<lines;i++)
+    fgets(s[i],LEN-1,f);
+  fclose(f);
+  return;
+}
+
+
+int miniRPSLS(Interface *i){
+  char **com;
+  int j,enemy=0,points=0,aux,col;
+    
+  /*Reservamos memoria para un array de strings*/  
+  com=(char **)malloc(sizeof(char *)*lines);
+  if(com==NULL)
+    return -1;
+  for(j=0;j<lines;j++){
+    com[j]=(char *)malloc(sizeof(char)*LEN);
+    if(com[j]==NULL){
+      j--;
+      while(j>=0){
+        free(com[j]);
+        j--;
+      }
+      free(com);
+      return -1;
+    }
+  }
+  /*we initialize the screen*/
+  i_cleanDisplay(i);
+  i_readFile(i,INI_PATH,0,0,1);
+  i_readFile(i,RPSLS_INST_PATH,0,0,2);
+  read_sols(com);/*read lines for round plus tie text*/
+
+  for(j=0;j<3;j++){
+    i_cleanCommand(i);
+    i_drawStrMap(i,com[j],1,1,3);/*round line*/
+    aux=_round(i);
+    i_cleanCommand(i);/*we clean to draw resul*/
+    /*we draw result in column*/
+    switch(j){
+      case 0:
+        col=16;
+        break;
+      case 1:
+        col=48;
+        break;
+      case 2:
+        col=87;
+        break;
+    }
+    if(aux==1){/*win*/
+      points++;
+      i_readFile(i,TICK_PATH,31,col,1);
+    }
+    if(aux==0){/*lose*/
+      enemy++;
+      i_readFile(i,CROSS_PATH,31,col,1);
+    }
+    if(aux==2){/*Tie*/
+      i_drawStrMap(i,com[3],31,col,1);
+    }
+    sleep(WAIT);
+  }
+  
+  /*Free memory*/
+  for(j=0;j<lines;j++){
+    free(com[j]);
+  }
+  free(com);
+  
+  if(points>enemy)
+    return 1;
+  else if(enemy>points)
+    return 0;
+  else
+    return 2;
 }
--- Codigo/Minigames/MiniRPSLS/miniRPSLS.h
+++ Codigo/Minigames/MiniRPSLS/miniRPSLS.h
@@ -8,12 +8,16 @@
 #define ROCK_PATH "DATA/miniInst/RPSLS/ROCK"
 #define PAPER_PATH "DATA/miniInst/RPSLS/PAPER"
 #define RPSLS_INST_PATH "DATA/miniInst/RPSLS/InstructionsRPSLS"
-#define WAIT 15000
+#define TICK_PATH "DATA/miniInst/RPSLS/TICK"
+#define CROSS_PATH "DATA/miniInst/RPSLS/CROSS"
+#define ROUND_PATH "DATA/miniInst/RPSLS/ROUNDS"
+#define WAIT 5 /*sleep time in seconds between rounds*/
 
 /*
   Returns 1 in case the player wins the minigame
   Returns 0 in case the player loses the minigame
   Returns 2 in case the player ties
+  REturns -1 if error
 */
 int miniRPSLS(Interface *i);
 
