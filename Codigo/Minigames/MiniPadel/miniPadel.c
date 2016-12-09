04b17709016ca56e1450d8991b20e54a6e9e542a
RodrigoDePool <rodrigodp05@gmail.com>
2016-12-09 17:50:34 +0100

    MiniPAdel

---
+++ Codigo/Minigames/MiniPadel/miniPadel.c
@@ -0,0 +1,24 @@
+#include "miniPadel.h"
+
+
+typedef struct _Ball
+{
+}Ball;
+
+typedef struct _Pad
+{
+}Pad;
+
+
+
+
+/*
+    Minigame, reference in .h
+ */
+int miniPadel(Interface * i, int hardMode)
+{
+    if (i == NULL)
+    {
+        return -1;
+    }
+}
