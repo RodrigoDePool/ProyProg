f48eadf2a1b7cd30699897d2b89911351233bb38
luciaasen <luciaasen@gmail.com>
2016-12-25 21:11:09 +0100

    Carlito halluda

---
+++ Codigo/Minigames/Mini2048/main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <time.h>
+#include <termios.h>
+#include "../../interface.h"
+
+
+
+int main(){
+  Interface *i;
+  
+	_term_init();/*Initializes the terminal for the interface*/
+	i = i_create(MAXCOLS - 30, MAXROWS - 6, 30, 6, '@', 40, 37, 40, 37, 40, 37);
+	
+  	i_drawAll(i);
+  	i_readFile(i, "board2048.txt", 0, 0, 1);
+  	i_drawAll(i);
+	
+  i_free(i);
+  tcsetattr(fileno(stdin), TCSANOW, &initial);
+  return 0;
+
+}
