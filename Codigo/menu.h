2e3a826419c0cde428599e966ba09c06d1492f6e
RodrigoDePool <rodrigodp05@gmail.com>
2016-12-26 20:35:27 +0100

    cambios basicos

---
+++ Codigo/menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+#include "interface.h"
+
+/*
+    This function is in charge of all the menu proccess. This includes
+    the player selecting a new game (or a saved one) then initializing
+    the world in a
+
+
+ */
+World *menu();
+
+#endif
