15c4ba65b9360816e464ee4c4e1f3813753a12d7
RodrigoDePool <rodrigodp05@gmail.com>
2016-12-27 00:46:10 +0100

    agregado bordes al 2048

--- Codigo/Minigames/Mini2048/test2048.c
+++ Codigo/Minigames/Mini2048/test2048.c
@@ -3,18 +3,19 @@
 #include <stdlib.h>
 
 
-int main(){
-	Interface *i;
-	int won;
-	
-	_term_init();
-	i = i_create(MAXCOLS - 30, MAXROWS - 6, 30, 6, '@', 40, 37, 40, 37, 40, 37);
-	if(!i) return -1;
-	
-	won = mini2048(i, "./data/numbers.txt", 4);
-	sleep(2);
-	i_free(i);
-	_term_close();
-	return won;
-	
+int main()
+{
+    Interface *i;
+    int       won;
+
+    _term_init();
+    i = i_create(MAXCOLS - 30, MAXROWS - 6, 30, 6, '@', 40, 37, 40, 37, 40, 37);
+    if (!i)
+        return -1;
+    i_drawAll(i);
+    won = mini2048(i, "./data/numbers.txt", 4);
+    sleep(2);
+    i_free(i);
+    _term_close();
+    return won;
 }
