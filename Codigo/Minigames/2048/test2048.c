e55484bf86be2269678351f3c53af4219f27a8b3
luciaasen <luciaasen@gmail.com>
2016-12-31 01:59:26 +0100

    Desastre arreglado?

--- Codigo/Minigames/2048/test2048.c
+++ Codigo/Minigames/2048/test2048.c
@@ -10,7 +10,7 @@ int main(){
 	_term_init();
 	i = i_create(MAXCOLS - 30, MAXROWS - 6, 30, 6, '@', 40, 37, 40, 37, 40, 37);
 	if(!i) return -1;
-	
+	i_drawAll(i);
 	won = mini2048(i, "./data/numbers.txt", 4);
 	sleep(2);
 	i_free(i);
