9220aff5496a5f8739c06077cc0f4c18ed73c085
luciaasen <luciaasen@gmail.com>
2017-01-16 02:23:11 +0100

    Avanzo un poquin con estos dos modulos, cosas q preguntar a juanete :D

--- Codigo/game.c
+++ Codigo/game.c
@@ -3,7 +3,9 @@
 #include <assert.h>
 #include "game.h"
 #define STRPATH
-
+#define INIROW 2
+#define INICOL 2
+#define SLEEPTIME 2
 
 struct _Strings{
 	int nums;
@@ -29,7 +31,7 @@ String *string_ini(){
 		return NULL;
 	}
 	for(i = 0;i < s->ns; i++){
-		fscanf(f, "%m[^\n]\n", (s->st)+i);
+		fscanf(f, "%m[^|]|\n", (s->st)+i);
 		if( s->st[i] == NULL ){
 			string_free(s);
 			fclose(f);
@@ -57,21 +59,63 @@ void string_free(String *s){
 /*Gives a clue to the space riddle if the player has all the space objects
   Else, tells the player that he cannot be helped
   Returns -1 if anything went wrong*/
-int game_help(World *w){
-	assert(w);
+int game_help(World *w, String *s){
+	assert(w && s);
 	Interface *in = world_getInterface(w);
 	if(!in) return -1;
-	if(world_getNumObjects != 3){
-		
+	if(world_getNumObjects(w) != 3){
+		i_drawStr(in, s->st[0], INIROW , INICOL , 2);
+		sleep(SLEEPTIME);
+	}else{
+	/*Level get clue?*/
 	}
-	
+	return 0;
 }
 
 /*Saves the game and shows message
-  Returns¿?*/
-int game_save(World *w);
+  Returns -1 if something went wrong, else 1*/
+int game_save(World *w, String* s){
+	assert(w && s);
+	Interface *in = world_getInterface(w);
+	if(!in) return -1;
+	if(world_saveToFile(w, SAVEPATH) == -1){
+		i_drawStr(in, s->st[1], INIROW , INICOL , 2);
+		sleep(SLEEPTIME);
+		return -1;
+	}else{
+		i_drawStr(in, s->st[2], INIROW , INICOL , 2);
+		sleep(SLEEPTIME);
+		return 0;
+	}
+}
 
-
+int game_solve(World *w, String *s){
+	assert(w && s);
+	Interface *in = world_getInterface(w);
+	char *solution;
+	Level *l;
+	if(!in) return -1;
+	
+	/*"Enter the solution" string*/
+	i_drawStr(in, s->st[3], INIROW , INICOL , 2);
+	
+	/* Meto en solution la solucion del jugador... ¿habra que quitar el intro?*/
+	/*Se puede resolver sin tener todos los objetos?*/
+	/*Como obtienes el level??*/	
+	l = world_getLevel(w);
+	if(!l) return -1;
+	if( strcmp(solution, l->solution) == 0 ){
+		/*You passed level string*/
+		i_drawStr(in, s->st[4], INIROW , INICOL , 2);
+		sleep(SLEEPTIME);
+		return 0;
+	}else{
+		/*Youre wrong string*/
+		i_drawStr(in, s->st[2], INIROW , INICOL , 2);
+		sleep(SLEEPTIME);
+		return 0;
+	}	
+}
 
 
 int game_f(World *w, int n){
@@ -81,11 +125,11 @@ int game_f(World *w, int n){
 		return -1;
 	
 	if(n == 0){
-	
+		return game_help(w, s);
 	}else if(n == 1){
-	
+		return game_save(w, s);
 	}else if(n == 2){
-	
+		return game_solve(w, s);
 	}
 }
 
--- Codigo/game.h
+++ Codigo/game.h
@@ -5,8 +5,17 @@
 
 
 typedef struct _Strings Strings;
+/* Strings file structure
+ 3 #nstrings
+ I'm sorry, you need to have the three parts if you wanna have a clue!|
+ Everything was successfuly saved.|
+ Enter the solution to the riddle|
+
+*/
+
 /*PENSEMOS FUNCIONES*/
 
 /*Executes help if n = 0, save if n = 1, solve if n = 2
-  Returns -1 in case of error*/
+  Returns -1 in case of error (memory error, couldnt be saved...)
+  or 0 if nothing went wrong*/
 int game_f(World *w, int n);
