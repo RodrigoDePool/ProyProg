6b959fe364c9b54fc05f608d4ea3685c5492f336
Carlos <carlosisamar@gmail.com>
2017-01-16 11:47:52 +0100

    animations en proceso

---
+++ Codigo/Animations/animations.h
@@ -0,0 +1,18 @@
+#include "../interface.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void One2Two_animation(Interface *i);
+
+void Two2Three_animation(Interface *i);
+
+void Three2Four_animation_LucyFight(Interface *i);
+/*
+void Bat_animation(Interface *i);
+
+void Lucia_animation(Interface *i);
+
+void Ringing_animation(Interface *i);
+
+void Luisa_animation(Interface *i);
+*/
